0204c8df11d45d8009bd434d4875707a03af27b6
Shivang Kaul <shangkaul@gmail.com>
2022-05-06 13:54:12 +0530

    local array search filter working

--- src/Nav.js
+++ src/Nav.js
@@ -1,29 +1,63 @@
-import './App.css';
-import {Link} from "react-router-dom";
-import {useProductFilter} from "./context/productFilterContext";
+import "./App.css";
+import { Link } from "react-router-dom";
+import { useProductFilter } from "./context/productFilterContext";
 
 function Nav() {
   const {
     filterState: { searchKeyword },
-    filterStateDispatch,
+    filterStateDispatch
   } = useProductFilter();
 
   return (
-      <nav>
-        <div className="nav-bar">
+    <nav>
+      <div className="nav-bar">
         <ul className="nav-group start">
-        <li> <Link to="/"> <h2 className="logo">Shop - olio</h2></Link></li>
+          <li>
+            {" "}
+            <Link to="/">
+              {" "}
+              <h2 className="logo">Shop - olio</h2>
+            </Link>
+          </li>
         </ul>
-        <div className="search-bar"><input className="textbox" placeholder="Search" value={searchKeyword} 
-        onChange={(e) =>
-          filterStateDispatch({ type: "SEARCH", payload: e.target.value })}/> <button className="btn search-btn"><i className="fa fa-search"></i></button></div>
+        <div className="search-bar">
+          <input
+            className="textbox"
+            placeholder="Search"
+            value={searchKeyword}
+            onChange={(e) =>
+              filterStateDispatch({ type: "SEARCH", payload: e.target.value })
+            }
+          />{" "}
+          <button className="btn search-btn">
+            <i className="fa fa-search"></i>
+          </button>
+        </div>
         <ul className="nav-group end">
-        <li> <Link to="/login"><button className="btn btn-sleek"> Login </button></Link></li>
+          <li>
+            {" "}
+            <Link to="/login">
+              <button className="btn btn-sleek"> Login </button>
+            </Link>
+          </li>
 
-        <li><Link to="/wishlist"><i className="icon icon-sm fa fa-heart"><span className="badge badge-sm badge-inactive">3</span></i></Link></li>
-        <li><Link to="/cart"><i className="icon icon-sm fa fa-shopping-cart"><span className="badge badge-sm badge-inactive">3</span></i></Link></li>
-        </ul></div>
-        </nav> 
+          <li>
+            <Link to="/wishlist">
+              <i className="icon icon-sm fa fa-heart">
+                <span className="badge badge-sm badge-inactive">3</span>
+              </i>
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart">
+              <i className="icon icon-sm fa fa-shopping-cart">
+                <span className="badge badge-sm badge-inactive">3</span>
+              </i>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
--- src/listing/Listing.js
+++ src/listing/Listing.js
@@ -1,103 +1,132 @@
-import { useEffect,useState } from 'react';
-import '../App.css';
-import './listing.css';
-import axios from 'axios';
-import { Link } from 'react-router-dom';
-
+import { useEffect, useState } from "react";
+import "../App.css";
+import "./listing.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 function Listing() {
+  const [itemList, setItemList] = useState([]);
+  const [defaultList, setDefaultList] = useState([]);
+  const [filterFlag, setFilterFlag] = useState(true);
 
-    const [itemList,setItemList]=useState([]);
-    const [filterList,setFilterList]=useState([]);
+  const toggleFilterFlag = () => setFilterFlag(!filterFlag);
 
-    useEffect(()=>{
-        const get_items = async () =>{
-          try{
-          let res= await axios.get("/api/products");
-          setItemList(itemList.concat(res.data.products));
-          setFilterList(res.data.products);
-      
-      }
-      catch(err){
+  useEffect(() => {
+    const get_items = async () => {
+      try {
+        let res = await axios.get("/api/products");
+        setItemList(itemList.concat(res.data.products));
+        setDefaultList(defaultList.concat(res.data.products));
+      } catch (err) {
         console.log(err);
       }
-      }
-      get_items();
-      },[])
+    };
+    get_items();
+  }, []);
 
-      console.log(filterList);
-const handleRadio = (e) => {
-    var name=e.target.name;
-    var val=e.target.value;
+  useEffect(() => {
+    let filterParams = JSON.parse(localStorage.getItem("filterState"));
+    setItemList(
+      filterParams.searchKeyword === ""
+        ? [...defaultList]
+        : itemList.filter((obj) =>
+            obj["title"]
+              .toLowerCase()
+              .includes(filterParams.searchKeyword.toLowerCase())
+          )
+    );
+  }, [filterFlag]);
 
-    console.log(name,val);
-    setFilterList(itemList.filter((obj)=> eval(obj[name].toString() + val)));
-}
+  console.log(itemList);
+  const handleRadio = (e) => {
+    var name = e.target.name;
+    var val = e.target.value;
+
+    console.log(name, val);
+    setItemList(itemList.filter((obj) => eval(obj[name].toString() + val)));
+  };
   return (
     <div className="main-body">
-        <div className="grid grid-sidemenu">
-            <aside className="grid-item grid-menu">
-                <div>
-                    <h3>Price</h3>
-                    <div className="check-grp" onChange={(e)=>handleRadio(e)}>
-                    <input type="radio" name="price" value="<1000"/> Less than ₹1000 <br/>
-                    <input type="radio" name="price" value=">=1000"/> ₹1000 +<br/>
-                    <input type="radio" name="price" value=">=1500"/> ₹1500 +<br/>
-                    <input type="radio" name="price" value=">=2000"/> ₹2000 +<br/>
-                
-                </div>
-                </div>
-                <div>
-                    <h3>Category</h3>
-                    <div className="check-grp">
-                        <input type="checkbox" name="category" value="Men"/> Men<br/>
-                        <input type="checkbox" name="category" value="Women"/> Women<br/>
-                        <input type="checkbox" name="category" value="Kids"/> Kids<br/>
-                        <input type="checkbox" name="category" value="Kids"/> Footwear<br/>
-                    
-                    </div>
-                </div>
-                <div>
-                    <h3>Rating</h3>
-                    <div className="check-grp" onChange={(e)=>handleRadio(e)}>
-                        <input type="radio" name="rating" value=">=1"/> <i className="menu-star fa fa-star"></i>+<br/>
-                        <input type="radio" name="rating" value=">=2"/> <i className="menu-star fa fa-star"></i><i className="menu-star fa fa-star"></i>+<br/>
-                        <input type="radio" name="rating" value=">=3"/> <i className="menu-star fa fa-star"></i><i className="menu-star fa fa-star"></i><i className="menu-star fa fa-star"></i>+<br/>
-                        <input type="radio" name="rating" value=">=4"/> <i className="menu-star fa fa-star"></i><i className="menu-star fa fa-star"></i><i className="menu-star fa fa-star"></i><i className="menu-star fa fa-star"></i>+<br/>
-                    
-                    </div>
-                </div>
-                <div>
-                    <h3>Sort By</h3>
-                    <div className="check-grp">
-                        <input type="radio" name="sort" value="Rating"/> Rating<br/>
-                        <input type="radio" name="sort" value="Price"/> Price<br/>                
-                    </div>
-                </div>
-            </aside>
-            <section className="main-container grid-item">
-            {
-      filterList.map((item)=>
-
-        (<div className="card img-card product-card" key={item._id}>
-        <span className="badge btn-round-md"><i className="fa fa-heart"></i></span>
-        <div className="card-img">
-        <img alt="product image" src={item.img_url} />
-   </div>
-        <div className="card-content">
-        <p className="sm">{item.title}</p>
-        <h3>₹{item.price}</h3>
-        </div>
-        <div className="card-footer">
-        <button className="btn btn-full-width"> Add to Cart</button>
-        <button className="btn btn-full-width btn-success"> Add to Wishlist</button>
-        </div>
-        </div>))
-        
-      }
+      <div className="grid grid-sidemenu">
+        <aside className="grid-item grid-menu">
+          <div>
+            <h3>Price</h3>
+            <div className="check-grp" onChange={(e) => handleRadio(e)}>
+              <input type="radio" name="price" value="<1000" /> Less than ₹1000{" "}
+              <br />
+              <input type="radio" name="price" value=">=1000" /> ₹1000 +<br />
+              <input type="radio" name="price" value=">=1500" /> ₹1500 +<br />
+              <input type="radio" name="price" value=">=2000" /> ₹2000 +<br />
+            </div>
+          </div>
+          <div>
+            <h3>Category</h3>
+            <div className="check-grp">
+              <input type="checkbox" name="category" value="Men" /> Men
+              <br />
+              <input type="checkbox" name="category" value="Women" /> Women
+              <br />
+              <input type="checkbox" name="category" value="Kids" /> Kids
+              <br />
+              <input type="checkbox" name="category" value="Kids" /> Footwear
+              <br />
+            </div>
+          </div>
+          <div>
+            <h3>Rating</h3>
+            <div className="check-grp" onChange={(e) => handleRadio(e)}>
+              <input type="radio" name="rating" value=">=1" />{" "}
+              <i className="menu-star fa fa-star"></i>+<br />
+              <input type="radio" name="rating" value=">=2" />{" "}
+              <i className="menu-star fa fa-star"></i>
+              <i className="menu-star fa fa-star"></i>+<br />
+              <input type="radio" name="rating" value=">=3" />{" "}
+              <i className="menu-star fa fa-star"></i>
+              <i className="menu-star fa fa-star"></i>
+              <i className="menu-star fa fa-star"></i>+<br />
+              <input type="radio" name="rating" value=">=4" />{" "}
+              <i className="menu-star fa fa-star"></i>
+              <i className="menu-star fa fa-star"></i>
+              <i className="menu-star fa fa-star"></i>
+              <i className="menu-star fa fa-star"></i>+<br />
+            </div>
+          </div>
+          <div>
+            <h3>Sort By</h3>
+            <div className="check-grp">
+              <input type="radio" name="sort" value="Rating" /> Rating
+              <br />
+              <input type="radio" name="sort" value="Price" /> Price
+              <br />
+            </div>
+          </div>
+          <br />
+          <button onClick={toggleFilterFlag}>Submit</button>
+        </aside>
+        <section className="main-container grid-item">
+          {itemList.map((item) => (
+            <div className="card img-card product-card" key={item._id}>
+              <span className="badge btn-round-md">
+                <i className="fa fa-heart"></i>
+              </span>
+              <div className="card-img">
+                <img alt="product image" src={item.img_url} />
+              </div>
+              <div className="card-content">
+                <p className="sm">{item.title}</p>
+                <h3>₹{item.price}</h3>
+              </div>
+              <div className="card-footer">
+                <button className="btn btn-full-width"> Add to Cart</button>
+                <button className="btn btn-full-width btn-success">
+                  {" "}
+                  Add to Wishlist
+                </button>
+              </div>
+            </div>
+          ))}
         </section>
-    </div>
-    
+      </div>
     </div>
   );
 }
